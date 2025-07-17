796ea5ba68c7141d115603b55a7307f99ea3f2b7
SIRAJUDDIN <shaikhsirajuddin7859@gmail.com>
2025-07-17 23:08:18 +0530

    initial commit

---
+++ src/main/java/com/thryve/pgfinder/PgFinderApplication.java
@@ -0,0 +1,13 @@
+package com.thryve.pgfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thryve/pgfinder/PgFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thryve.pgfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
